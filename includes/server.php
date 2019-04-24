eb4728bbe42d95967134970d93ade75d446260f2
Marianela <marianela_min@yahoo.com>
2019-04-24 12:32:27 -0500

    fixed leaking issue

--- includes/server.php
+++ includes/server.php
@@ -1,13 +1,2 @@
 <?php
-
-    //connect to the database
-    $db_host = "140.254.14.102"; // ip address where the DB is running at
-    $db_name = "db_scholarhip_project";
-    $db_user = "user_scholar";
-    $db_pass = "Password123!";
-    $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-    // check connection
-    if ($mysqli->connect_error){
-        die('Connect Error: ' . $mysqli->connect_error);
-    }
-
+require_once("serverConfig.php");
