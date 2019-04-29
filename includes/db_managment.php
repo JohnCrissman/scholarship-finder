e3ce6bf9bf5210b180d9202c5bd3648574a703d8
John Crissman <JohnMCrissmanUSMC@gmail.com>
2019-04-29 14:12:10 -0500

    update show_scholarship method

--- includes/db_managment.php
+++ includes/db_managment.php
@@ -12,7 +12,7 @@ function greeting($name){
 
 function show_scholarship($db_handler){
     $html_read = "";
-    $sql="SELECT * FROM Scholarship";
+    $sql="call ScholarshipAndSponsor";
 
     $result = $db_handler->query($sql);
     // echo '<pre>';
@@ -25,7 +25,38 @@ function show_scholarship($db_handler){
             // echo '<pre>';
             // echo var_dump($row);
             // echo '</pre>';
-            
+           $html_read.=' <div class="card col-lg-6" >
+           <!-- <img src="..." class="card-img-top" alt="..."> -->
+           <div class="card-body">
+             <h5 class="card-title">'. $row['title'].'</h5>
+             <h6 class="card-subtitle mb-2 text-muted">'. $row['spCompanyName'].'</h6>
+             <p class="card-text">Min requirements:  '. $row['minRequirements'].' </p>
+           </div>
+           <ul class="list-group list-group-flush">
+             <li class="list-group-item">Minimum GPA:  '. $row['gpa'].'</li>
+             <li class="list-group-item">Award:  '. $row['amount'].'</li>
+             <li class="list-group-item">Deadline:  '. $row['deadline'].' </li>
+           </ul>
+           <div class="card-body">
+               <a href="#" class="card-link">Card link</a>
+               <a href="#" class="card-link">Another link</a>
+               <form action="" method="POST">
+                       <span style="float:right;">
+                           <!-- <button type="submit" name="del-submit" class="btn btn-danger"><i class="far fa-trash-alt"></i></button> -->
+                           <input type="number" value="'. $row['scholarshipID'].'" name="scholarshipID" readonly hidden>
+                           <button type="submit" name="like-submit" class="btn btn-success"><i class="far fa-heart"></i></button>
+                           <!-- <button type="submit" name="olike-submit" class="btn btn-danger"><i class="fas fa-heart"></i></button> -->
+                       </span>
+               </form>
+           </div>
+   </div>';
+           
+           
+           
+           
+           
+           
+            /* 
             $html_read = $html_read.'<li>';
             
             $html_read.= $row['scholarshipID'].'  - ';
@@ -39,6 +70,7 @@ function show_scholarship($db_handler){
             $html_read.= 'regDay: '.$row['regDay'];
             $html_read.= '<br/><br/>';
                 $html_read = $html_read.'</li>';
+            */
         }
     }else{
             die('Invalid query: ' . $db_handler->error());
@@ -62,20 +94,33 @@ function show_sponsor($db_handler){
             // echo '<pre>';
             // echo var_dump($row);
             // echo '</pre>';
+           $html_read.=' <div class="card col-lg-6" >
+           <!-- <img src="..." class="card-img-top" alt="..."> -->
+           <div class="card-body">
+             <h5 class="card-title">'. $row['title'].'</h5>
+             <h6 class="card-subtitle mb-2 text-muted">'. $row['spCompanyName'].'</h6>
+             <p class="card-text">Min requirements:  '. $row['minRequirements'].' </p>
+           </div>
+           <ul class="list-group list-group-flush">
+             <li class="list-group-item">Minimum GPA:  '. $row['gpa'].'</li>
+             <li class="list-group-item">Award:  '. $row['amount'].'</li>
+             <li class="list-group-item">Deadline:  '. $row['deadline'].' </li>
+           </ul>
+           <div class="card-body">
+               <a href="#" class="card-link">Card link</a>
+               <a href="#" class="card-link">Another link</a>
+               <form action="" method="POST">
+                       <span style="float:right;">
+                           <!-- <button type="submit" name="del-submit" class="btn btn-danger"><i class="far fa-trash-alt"></i></button> -->
+                           <input type="number" value="'. $row['scholarshipID'].'" name="scholarshipID" readonly hidden>
+                           <button type="submit" name="like-submit" class="btn btn-success"><i class="far fa-heart"></i></button>
+                           <!-- <button type="submit" name="olike-submit" class="btn btn-danger"><i class="fas fa-heart"></i></button> -->
+                       </span>
+               </form>
+           </div>
+   </div>';
+
             
-            $html_read = $html_read.'<li>';
-            
-            $html_read.= $row['scholarshipID'].'  - ';
-            $html_read.= $row['title'].' '.$row['amount'].'<br/>applyURL: ';
-            $html_read.= $row['applyURL'].'<br/>';
-            $html_read.= 'deaDLINE: '.$row['deadline'];
-            $html_read.= 'minReq: '.$row['minRequirements'];
-            $html_read.= 'gpa: '.$row['gpa'];
-            $html_read.= '<br/>status: '.$row['u_status'];
-            $html_read.= 'sponsorID: '.$row['sponsorID'];
-            $html_read.= 'regDay: '.$row['regDay'];
-            $html_read.= '<br/><br/>';
-                $html_read = $html_read.'</li>';
         }
     }else{
         echo "bad";
